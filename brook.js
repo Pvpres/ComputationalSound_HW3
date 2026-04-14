06c6e4eae4edc113c897c09c19dd98cc36342ad4
Preston Pressoir <123640756+Pvpres@users.noreply.github.com>
2026-04-14 16:43:10 -0400

    Brook finished

--- brook.js
+++ brook.js
@@ -1,7 +1,13 @@
 let audioCtx = null;
 let isPlaying = false;
 
-let brownNoise1, brownNoise2, lpf1, lpf2, modGain, rhpf, outputGain;
+let brownNoise1;
+let brownNoise2;
+let lpf1;
+let lpf2;
+let modGain;
+let rhpf;
+let outputGain;
 
 function createBrownNoiseBuffer(ctx) {
   var bufferSize = 10 * ctx.sampleRate,
@@ -18,3 +24,70 @@ function createBrownNoiseBuffer(ctx) {
   }
   return noiseBuffer;
 }
+
+function createBrownNoise() {
+  brownNoise1 = audioCtx.createBufferSource();
+  brownNoise1.buffer = createBrownNoiseBuffer(audioCtx);
+  brownNoise1.loop = true;
+
+  brownNoise2 = audioCtx.createBufferSource();
+  brownNoise2.buffer = createBrownNoiseBuffer(audioCtx);
+  brownNoise2.loop = true;
+}
+
+function startBrook(){
+  audioCtx = new AudioContext();
+  createBrownNoise();
+  //filters frequencies above 400 like in Supercollider example
+  lpf1 = audioCtx.createBiquadFilter();
+  lpf1.type = "lowpass";
+  lpf1.frequency.value = 400;
+
+  //control frequency
+  lpf2 = audioCtx.createBiquadFilter();
+  lpf2.type = "lowpass";
+  lpf2.frequency.value = 14;
+   modGain = audioCtx.createGain();
+  modGain.gain.value = 1300;
+
+  // RHPF.ar(..., freq, rq: 0.03, mul: 0.1)
+  rhpf = audioCtx.createBiquadFilter();
+  rhpf.type = "highpass";
+  rhpf.frequency.value = 500;
+  rhpf.Q.value = 1 / 0.03; 
+
+  // mul: 0.1
+  outputGain = audioCtx.createGain();
+  outputGain.gain.value = 0.1;
+
+  brownNoise1.connect(lpf1);
+  lpf1.connect(rhpf);
+  rhpf.connect(outputGain);
+  outputGain.connect(audioCtx.destination);
+
+  brownNoise2.connect(lpf2);
+  lpf2.connect(modGain);
+  modGain.connect(rhpf.frequency);
+
+  brownNoise1.start(0);
+  brownNoise2.start(0);
+
+}
+
+function stopBrook() {
+  if (audioCtx) {
+    audioCtx.close();
+    audioCtx = null;
+  }
+}
+
+document.getElementById("toggleBtn").addEventListener("click", () => {
+  if (isPlaying) {
+    stopBrook();
+    document.getElementById("toggleBtn").textContent = "Play";
+  } else {
+    startBrook();
+    document.getElementById("toggleBtn").textContent = "Stop";
+  }
+  isPlaying = !isPlaying;
+});
